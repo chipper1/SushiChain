3246266aa1d0d65068c68f8da21b8345a54674b6
tbrand <taichiro0709@gmail.com>
2018-05-10 23:27:03 +0900

    [skip ci] wip

--- src/core/dapps/dapp.cr
+++ src/core/dapps/dapp.cr
@@ -12,6 +12,8 @@
 
 module ::Sushi::Core::DApps
   abstract class DApp
+    alias NodeContext = NodeComponents::Chord::NodeContext
+
     abstract def setup
     abstract def transaction_actions : Array(String)
     abstract def transaction_related?(action : String) : Bool
@@ -24,6 +26,11 @@ module ::Sushi::Core::DApps
       context : HTTP::Server::Context,
       params : Hash(String, String)
     ) : HTTP::Server::Context?
+
+    #
+    # receive a message from other nodes
+    #
+    abstract def on_message(msg : String, from : NodeContext)
 
     def initialize(@blockchain : Blockchain)
     end
@@ -40,12 +47,35 @@ module ::Sushi::Core::DApps
     end
 
     #
-    # Default fee is 1 SHARI
-    # All thrid party dApps cannot override here.
-    # Otherwise the transactions will be rejected from other nodes.
+    # default fee is 1 SHARI
+    # all thrid party dApps cannot override here.
+    # otherwise the transactions will be rejected from other nodes.
     #
     def self.fee(action : String) : Int64
       1_i64
+    end
+
+    #
+    # send a message to specific node
+    # specify a node by node id
+    #
+    def send_message(msg : String, to : String)
+      # todo
+    end
+
+    #
+    # send a mesage to specific nodes
+    # specify nodes by an array of node ids
+    #
+    def send_message(msg : String, to : Array(String))
+      # todo
+    end
+
+    #
+    # send a message to every nodes connected
+    #
+    def send_message_broadcast(msg : String)
+      # todo
     end
 
     private def blockchain : Blockchain
